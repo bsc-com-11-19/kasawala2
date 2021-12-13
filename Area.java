31f8a07c678d569152c94e5752c87496a4f468e9
bsc-com-11-19 <bsc-com-11-19@unima.ac.mw>
2021-12-13 17:50:16 +0200

    lab session 1

--- Area.java
+++ Area.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 public class Area{
     public static void main(String[] args){
         System.out.println("The area will be displayed here");
+        Scanner input=new Scanner(System.in);
+
+        //accepting length and breadth from the user
         System.out.println("Enter the length of the rectangle");
         double length=input.nextDouble();
         System.out.println("Enter the length of the breadth");
         double breadth=input.nextDouble();
+
+        //displaying the area
         System.out.println("The area is "+ReturnArea(length,breadth));
        
     }
+    //method to calculate area
     public static double ReturnArea(double l,double b){
         double area=l*b;
         return area; 
